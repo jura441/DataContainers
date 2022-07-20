17c49091c4f704f29dfab0509f8bce9316d086dd
jura441 <102172272+jura441@users.noreply.github.com>
2022-07-20 21:54:27 +0300

    Exceptions

--- List2/main.cpp
+++ List2/main.cpp
@@ -14,7 +14,8 @@ class List
 		Element* pNext;
 		Element* pPrev;
 	public:
-		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) :Data(Data), pNext(pNext), pPrev(pPrev)
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) 
+			:Data(Data), pNext(pNext), pPrev(pPrev)
 		{
 			cout << "EConstructor:\t" << this << endl;
 		}
@@ -23,10 +24,9 @@ class List
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class List;
-	};
-	Element* Head;
-	Element* Tail;
-	size_t size;
+	}*Head, *Tail;
+	unsigned int size;
+	//size_t size;
 public:
 	List()
 	{
@@ -36,35 +36,141 @@ public:
 	}
 	~List()
 	{
-
+		//while (Head) pop_front();
+		while (Tail)pop_back();
 		cout << "LDestructor:\t" << this << endl;
-
 	}
+
+	//			Adding Elements:
 
 	void push_front(int Data)
 	{
+		/*Element* New = new Element(Data);
 		if (Head == nullptr && Tail == nullptr)
 		{
-			Head = Tail = new Element(Data);
+			Head = Tail = New;
 		}
 		else
 		{
-			Element* New = new Element(Data);
 			New->pNext = Head;
 			Head->pPrev = New;
 			Head = New;
+		}*/
+		if (Head == nullptr && Tail == nullptr)
+			Head = Tail = new Element(Data);
+		else
+			Head = Head->pPrev = new Element(Data, Head);
+		size++;
+	}
+	void push_back(int Data)
+	{
+		/*Element* New = new Element(Data);
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = New;
 		}
+		else
+		{
+			New->pPrev = Tail;
+			Tail->pNext = New;
+			Tail = New;
+		}*/
+		if (Head == nullptr && Tail == nullptr)
+			Head = Tail = new Element(Data);
+		else
+			Tail = Tail->pNext = new Element(Data, nullptr, Tail);
 		size++;
 	}
 
-	void print()const
+	void insert(int Data, int index)
 	{
-		cout << "Адрес списка:" << Head << endl;
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
-			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-		cout << "Количество элементов списка: " << size << endl;
+		if (index > size)return;
+		if (index == 0)return push_front(Data);
+		if (index == size)return push_back(Data);
+
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+		}
+		/*Element* New = new Element(Data);
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;*/
+		Temp->pPrev = Temp->pPrev->pNext = new Element(Data, Temp, Temp->pPrev);
+		size++;
 	}
 
+	//				Removing Elements:
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Head = Head->pNext;
+			delete Head->pPrev;
+			Head->pPrev = nullptr;
+		}
+	}
+	void pop_back()
+	{
+			if (Head == Tail)return pop_front();
+			Tail = Tail->pPrev;
+			delete Tail->pNext;
+			Tail->pNext = nullptr;
+			size--;
+	}
+	
+	void erase(int index)
+	{
+		if (index >= size)return;
+		if (index == 0)return pop_front();
+		//1. Доходим до удаляемого элемента
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+		}
+		//2. Исключаем элемент из списка
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		//3. Удаляем элемент из списка
+		delete Temp;
+		size--;
+	}
+	//				Methods:
+	void print()const
+	{
+		cout << "Head:" << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов: " << size << endl;
+	}
+	void reverse_print()const
+	{
+		cout << "Tail:" << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов: " << size << endl;
+	}
 };
 
 void main()
@@ -76,6 +182,21 @@ void main()
 	for (int i = 0; i < n; i++)
 	{
 		list.push_front(rand() % 100);
+		//list.push_back(rand() % 100);
 	}
 	list.print();
+	list.reverse_print();
+
+	int value;
+	int index;
+	cout << "Введите значение добавляемого элемента:"; cin >> value;
+	cout << "Введите индекс добавляемого элемента:"; cin >> index;
+	list.insert(value, index);
+	list.print();
+	list.reverse_print();
+	cout << "Введите индекс удаляемого элемента:"; cin >> index;
+	list.erase(index);
+	list.print();
+	list.reverse_print();
+
 }
